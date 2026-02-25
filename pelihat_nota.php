6a7f3790ec47ab87ac3280786f20899caa9b07ce
m-8201066-tech <m-8201066@moe-dl.edu.my>
2026-02-25 21:51:33 +0800

    Update pelihat_nota.php

--- pelihat_nota.php
+++ pelihat_nota.php
@@ -1,10 +1,30 @@
 <?php
-$file = $_GET['file'];
+if (isset($_GET['file'])) {
+    $file = $_GET['file'];
+    // Pastikan folder 'uploads/' ada di depan nama fail
+    $path = "uploads/" . $file;
+} else {
+    echo "Fail tidak dinyatakan.";
+    exit;
+}
 ?>
-<body oncontextmenu="return false;">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nota</title>
     <style>
-        body { margin: 0; overflow: hidden; }
-        iframe { width: 100%; height: 100vh; border: none; }
+        body { margin: 0; padding: 0; overflow: hidden; background-color: #333; }
+        iframe { border: none; width: 100%; height: 100vh; }
     </style>
-    <iframe src="uploads/<?php echo $file; ?>#toolbar=0&navpanes=0&scrollbar=0"></iframe>
+</head>
+<body oncontextmenu="return false;">
+    <?php if (file_exists($path)): ?>
+        <iframe src="<?php echo $path; ?>#toolbar=0&navpanes=0&scrollbar=0"></iframe>
+    <?php else: ?>
+        <div style="color: white; text-align: center; padding-top: 50px;">
+            <h2>Ralat: Fail tidak dijumpai!</h2>
+            <p>Pastikan fail <b><?php echo htmlspecialchars($file); ?></b> berada di dalam folder <b>uploads/</b></p>
+        </div>
+    <?php endif; ?>
 </body>
+</html>
